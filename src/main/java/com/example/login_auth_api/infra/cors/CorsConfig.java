e32f9f62caaf24081ee37a0fc058f6b83fad142e
Enzo Hessel <enzo.carmo@salg.com.br>
2024-07-23 13:49:48 -0300

    configure cors

--- src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
+++ src/main/java/com/example/login_auth_api/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://login-jwt-bice.vercel.app/")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
